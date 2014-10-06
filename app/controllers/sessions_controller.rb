f7484f11e98cf1ff5c64601f91e385e0b3a94a59
Diana Hilton <diana.hilton@gmail.com>
2014-10-06 15:27:10 -0700

    Before database column drop.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 	def create
-        @user = User.find_by :username => params[:username]
+        @user = User.find_by :email => params[:email]
 
         if @user.nil?
             flash[:error] = "No such user."
