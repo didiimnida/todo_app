465b2747d8f52ed7abbcf044ec1244a9162a3f7b
Diana Hilton <diana.hilton@gmail.com>
2014-10-08 14:46:48 -0700

    Made changes to styling.  Also, commented out gem clockwork stuff. Going to try Heroku scheduler first.

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,8 +1,8 @@
-require File.expand_path('../../config/boot',        __FILE__)
-require File.expand_path('../../config/environment', __FILE__)
-require 'clockwork'
+# require File.expand_path('../../config/boot',        __FILE__)
+# require File.expand_path('../../config/environment', __FILE__)
+# require 'clockwork'
 
-include Clockwork
+# include Clockwork
 
-every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new }
-every(1.day, 'Queueing scheduled job', :at => '12:10') { Delayed::Job.enqueue ScheduledJob.new }
+# every(4.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new }
+# every(1.day, 'Queueing scheduled job', :at => '12:10') { Delayed::Job.enqueue ScheduledJob.new }
