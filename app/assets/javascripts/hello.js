c29c0b949218b8e4388bfa220433853fbb7c58d9
Diana Hilton <diana.hilton@gmail.com>
2014-10-07 17:40:37 -0700

    Moved Jquery to another file.

---
+++ app/assets/javascripts/hello.js
@@ -0,0 +1,72 @@
+
+$(function() {
+
+$('button').click(function(){
+	var desc = $('input').val();
+
+	data_hash = {
+		"todo":{"description": desc}
+	}
+
+	var request = $.ajax({
+		url: "/todos",
+		type: "POST",
+		data: data_hash,
+		dataType: "json"
+	});
+
+	request.done(function(data){
+		$('ul').append("<li data-todo-id= '" + data.id + "'>" + "<input type ='checkbox'> " + data.description + "<span>&times;</span></li>")
+		//console.log(data);
+		$('input').val() = "";
+	});
+
+	request.fail(function(data){
+		console.log("FAIL FUNCTION");
+		$('h1').append("<p> There was an error saving your todo</p>")
+	});
+
+});
+
+
+$('ul').click(function(e){
+  	console.log(e.target);
+  	var li = $(e.target).parent();
+	var id = li.data("todo-id");
+
+  	if($(e.target).is("span")){
+		var request = $.ajax({
+			url: "/todos/"+id,
+			type: "DELETE",
+			dataType: "json"
+		});
+
+		request.done(function(data){
+			li.remove();
+		});
+	}
+
+	if($(e.target).is("input")){
+		var checkbox_value = $(e.target).is(':checked');
+		
+		data_hash = {
+		"todo":{"completed":checkbox_value}
+		}
+
+		var request = $.ajax({
+			url: "/todos/"+ id,
+			type: "PATCH",
+			data: data_hash,
+			dataType: "json"
+		});
+
+		request.done(function(data){
+			console.log(data)
+
+		});
+	}
+
+	});
+});
+
+
